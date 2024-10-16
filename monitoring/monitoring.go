21876c28ce686c76a89b02f399489686dddcc137
Arthur bernardo <arthurutschgg@gmail.com>
2024-10-16 18:04:20 -0300

    monitoring

---
+++ monitoring/monitoring.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comand int
+	fmt.Scan(&comand)
+
+}

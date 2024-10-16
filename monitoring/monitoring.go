6e7defa8719b5862b1f3b525a3b7197ad9cdb60a
Arthur bernardo <arthurutschgg@gmail.com>
2024-10-16 18:12:44 -0300

    control flow

--- monitoring/monitoring.go
+++ monitoring/monitoring.go
@@ -7,7 +7,21 @@ func main() {
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
 
-	var comand int
-	fmt.Scan(&comand)
+	var command int
+	fmt.Scan(&command)
+
+	switch command {
+	case 0:
+		fmt.Println("Saindo...")
+		break
+	case 1:
+		fmt.Println("Monitorando...")
+		break
+	case 2:
+		fmt.Println("Exibindo logs...")
+		break
+	default:
+		fmt.Println("Nāo conheço esse comando")
+	}
 
 }

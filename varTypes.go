0c4953aed6eb8083e03d6373b9319561d7a3d179
Arthur bernardo <arthurutschgg@gmail.com>
2024-10-16 17:49:57 -0300

    var types

---
+++ varTypes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Arthur"
+	var version float32 = 1.1
+	var age int = 22
+	fmt.Println("ola Mundo", nome)
+	fmt.Println("programa na versao", version)
+	fmt.Println("idade", age)
+}

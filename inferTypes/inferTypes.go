db1816c7151daf9c3034e1709c4a48e75f6df35c
Arthur bernardo <arthurutschgg@gmail.com>
2024-10-16 17:54:43 -0300

    infer types

---
+++ inferTypes/inferTypes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Arthur"
+	version := 1.1
+	age := 22
+	fmt.Println("ola Mundo", nome)
+	fmt.Println("programa na versao", version)
+	fmt.Println("idade", age)
+}
